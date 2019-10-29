5675ae8d0e404b2b1d2d3d1a3410996aa0d14e6f
Michael Bottini <michael.a.bottini@intel.com>
2019-10-29 14:18:22 -0700

    Add assign4/exercise2

---
+++ assign4/exercise2.js
@@ -0,0 +1,16 @@
+function reverseNum(num) {
+	return parseInt(
+		String(num).split("")
+			.reverse()
+			.join(""))
+}
+
+function reverseNumArithmetic(num) {
+	return reverseNumArithmeticHelper(num, 0);
+}
+
+function reverseNumArithmeticHelper(num, acc) {
+	return num === 0 ?
+		acc :
+		reverseNumArithmeticHelper(Math.floor(num / 10), acc * 10 + num % 10);
+}
