6eba9141d7dc0b684a7f684e424cac5e5edac99e
Michael Bottini <michael.a.bottini@intel.com>
2019-10-29 14:06:51 -0700

    Add assign4/exercise1

---
+++ assign4/exercise1.js
@@ -0,0 +1,18 @@
+function fizzbuzz(num) {
+	let result = "";
+	if (num % 3 === 0) {
+		result += "fizz";
+	}
+	if (num % 5 === 0) {
+		result += "buzz";
+	}
+	return result.length !== 0 ? result : String(num);
+}
+
+function range(start, end) {
+	return [...Array(end - start).keys()].map(x => x + start);
+}
+
+function fizzbuzzPrintRange(start, end) {
+	range(start, end).map(fizzbuzz).forEach(elem => console.log(elem));
+}
