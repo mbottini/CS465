629936984b33f05fb2b40b0d75fea123f9ddf62b
Michael Bottini <mbottini1@gmail.com>
2019-11-22 18:12:20 -0800

    Clean up assign5

--- assign5/server.js
+++ assign5/server.js
@@ -7,7 +7,6 @@ app.use(express.urlencoded());
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
 app.get('/styles.css', (req, res) => res.sendFile(path.join(__dirname + '/styles.css')));
-app.get('/script.js', (req, res) => res.sendFile(path.join(__dirname + '/script.js')));
 app.post('/submit', (req, res) => res.send(`
 <p>name: ${req.body.name}</p>
 <p>email: ${req.body.email}</p>
